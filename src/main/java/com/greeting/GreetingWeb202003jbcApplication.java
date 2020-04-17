7bd9d1867bdd4d15bd2e06398f87797fafdfa00c
Sean Blessing <snblessing@gmail.com>
2020-04-17 13:43:11 -0400

    init commit

---
+++ src/main/java/com/greeting/GreetingWeb202003jbcApplication.java
@@ -0,0 +1,13 @@
+package com.greeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingWeb202003jbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingWeb202003jbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greeting/GreetingWeb202003jbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingWeb202003jbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
